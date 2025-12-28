9f21b3482e91f5accb3f8906c8a6b58e148fc0e3
Le Quang Hung <hunglq7@gmail.com>
2025-12-28 18:22:26 +0700

    28.12.2025-18h22

--- src/routes/PagesRoutes.jsx
+++ src/routes/PagesRoutes.jsx
@@ -12,7 +12,6 @@ const LoginPage = Loadable(lazy(() => import('views/auth/login/Login')));
 const RegisterPage = Loadable(lazy(() => import('views/auth/register/Register')));
 // error page
 const Page404 = Loadable(lazy(() => import('views/page404/Page404')));
-const Page500 = Loadable(lazy(() => import('views/page500/Page500')));
 // ==============================|| AUTH PAGES ROUTING ||============================== //
 
 const PagesRoutes = {
@@ -33,10 +32,6 @@ const PagesRoutes = {
         {
           path: '*',
           element: <Page404 />
-        },
-        {
-          path: 'page500',
-          element: <Page500 />
         }
       ]
     }
