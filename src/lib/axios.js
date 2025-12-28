7ddc3504d31c92d46e905ba2c0bf73689819b131
Le Quang Hung <hunglq7@gmail.com>
2025-12-28 16:05:58 +0700

    28.1.2025

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import { useAuthStore } from '/src/stores/useAuthStore';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : '/api',
+  baseURL: import.meta.env.MODE === 'development' ? 'https://backen-camera.onrender.com/' : '/api',
   withCredentials: true
 });
 
